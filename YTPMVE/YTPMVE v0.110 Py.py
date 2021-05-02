c1a08442cf4a7b51c480ef8ed5bb4dd5210444a8
Cantersoft <46493366+Cantersoft@users.noreply.github.com>
2021-05-02 16:04:43 -0400

    Add files via upload

---
+++ YTPMVE/YTPMVE v0.110 Py.py
@@ -0,0 +1,93 @@
+#YTPMVE v0.110 | © Matthew Hansen
+#04282021
+
+import tkinter.filedialog as tf
+import tkinter
+
+root = tkinter.Tk()
+root.withdraw()
+MIDI_filename = tf.askopenfilename(initialdir = "/",title= "Select MIDI file", filetypes =[('MIDI', '*.mid')])
+print("Opened", MIDI_filename)
+root.destroy()
+
+import mido
+import os
+from os import path
+
+
+try:
+    os.makedirs((path.expandvars(r'%USERPROFILE%/AppData/Local/Temp/YTPMVE/')))
+except:
+    pass
+
+MIDI_file=mido.MidiFile(MIDI_filename)
+YTPMVE_file= open(path.expandvars(r'%USERPROFILE%/AppData/Local/Temp/YTPMVE/timestamps.txt'), 'w')
+
+
+if not len(MIDI_file.tracks)-1 == 1:#reject files with more than one track or zero tracks
+    YTPMVE_file.write("TrackError"+"\n")
+    YTPMVE_file.write(str(len(MIDI_file.tracks)-1))
+    YTPMVE_file.close()
+    exit()
+
+    
+current_time=0
+start=0
+MIDI_time=[]
+
+note_starts=[]#All note start times (seconds)
+note_durations=[]#All note durations (seconds)
+        
+for msg in MIDI_file:#Change this so that we don't look at the if statement except the first few times
+    if msg.is_meta:
+        start=start+1
+        continue
+    else:
+        #print("Message:", msg)
+        current_time=float(msg.time)+current_time
+        MIDI_time.append(current_time)
+        if msg.type == "note_on":
+           print('Note Void: ', msg.time)
+           print('Note Start Time: ', current_time)
+
+           note_starts.append([current_time, msg.note])
+           note_durations.append("NULL")#We must have a 1:1 ratio of note_offs for note_ons. This will create a space in note_durations which will be filled later.
+
+        elif msg.type == "note_off":
+            print('Note Duration: ', msg.time)
+            print('Note End Time: ', current_time)
+            print()
+
+            #note_durations.append(msg.time)
+
+            for i in range(len(note_starts)-1, -1, -1):#Reverse search the note starts list and find the note_on message that was probably linked to this note_off
+                print()
+                print("   Note start reverse search:", note_starts[i][1])
+                if note_starts[i][1]== msg.note:
+                    list_match=i
+                    note_durations[list_match]=current_time-note_starts[i][0]
+                    print()
+                    print("   Matching note found in note_starts at index position", list_match)
+                    break
+               
+            
+            
+           
+            
+
+
+
+        
+#print(str(MIDI_time)) #Print all the note starts and lengths, optionally.
+#print()                
+#print("Note start times, note durations:",str(note_starts))#note start time, note number
+#print("Note durations: ",str(note_durations))#note end time
+
+for i, j in enumerate(note_starts):#i becomes a counter, and j becomes the corresponding value in note_starts
+
+    #print(note_starts[i][0])
+    #print(note_durations[i])    
+    YTPMVE_file.write(str(note_starts[i][0])+",")#Save first argument, note start time
+    YTPMVE_file.write(str(note_durations[i])+"\n")#Save second argument, note duration
+
+YTPMVE_file.close()
---
+++ YTPMVE/YTPMVE v0.110.cs
@@ -0,0 +1,86 @@
+//YTPMVE v0.110 | © Matthew Hansen
+//04282021
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using ScriptPortal.Vegas;
+
+
+public class EntryPoint
+{
+    Vegas currentVegasApp;
+	public void FromVegas(Vegas vegas)
+	{
+		
+		string[] YTPMVEFileNames = {"YTPMVE v0.110 Py.py", "YTPMVE v0.110 Py.exe"};
+		
+		try{
+			System.Diagnostics.Process.Start(@Directory.GetCurrentDirectory() + "\\Script Menu\\YTPMVE\\" + YTPMVEFileNames[0]).WaitForExit();//Initiate the Python script and wait for it to finish.
+		}
+		catch{
+			try{
+				System.Diagnostics.Process.Start(@Directory.GetCurrentDirectory() + "\\Script Menu\\YTPMVE\\" + YTPMVEFileNames[1]).WaitForExit();//Or initiate the executable and wait for it to finish.
+			}
+			catch{
+				MessageBox.Show("An error occurred while attempting to launch \"" + YTPMVEFileNames[0] + "\" or \"" + YTPMVEFileNames[1] + "\"! The file may be missing or named incorrectly.", "Error" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}	
+
+		}
+		
+		string path = Directory.GetCurrentDirectory();
+		
+		currentVegasApp = vegas;
+		TrackEvent currentEvent;
+		TrackEvent sourceEvent;		
+		string[] arrTimeCodesSource = System.IO.File.ReadAllLines(Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\Temp\YTPMVE\timestamps.txt"));
+		
+		List<String> arrTimeCodes = new List<String>();
+
+		String strDefaultEventDuration = "0.1";
+		String strCurrentEventStart = "0";
+		
+		bool timestampsContainsNulls = false;
+
+		if (Convert.ToString(arrTimeCodesSource[0]) == "TrackError"){
+			MessageBox.Show("An error occurred because there are " + Convert.ToString(arrTimeCodesSource[1]) + " tracks in the MIDI file you selected. Currently, this script only supports 1 track. Pls forgiv.", "Track Error" , MessageBoxButtons.OK, MessageBoxIcon.Error);
+			return;
+		}		
+		
+		for (int i = 0; i < arrTimeCodesSource.Length; i++){
+			if (arrTimeCodesSource[i].Contains("NULL")){
+				strCurrentEventStart = arrTimeCodesSource[i].Split(',')[0];
+				arrTimeCodes.Add(strCurrentEventStart + "," + strDefaultEventDuration);
+
+				currentVegasApp.Project.Markers.Add(new Marker(Timecode.FromSeconds(Double.Parse(strCurrentEventStart)), "NULL DURATION"));
+				timestampsContainsNulls = true;
+			}
+			else{
+				arrTimeCodes.Add(arrTimeCodesSource[i]);
+			}
+		}
+
+		if(timestampsContainsNulls){
+			MessageBox.Show("Some notes were overlapping or invalid, and thus their durations could not be determined. Markers have been added at such positions in the timeline.", "Warning" , MessageBoxButtons.OK, MessageBoxIcon.Warning);		
+		}
+		
+		
+		
+		for (int i = 0; i < currentVegasApp.Project.Tracks.Count; i++){
+			try{
+				sourceEvent = currentVegasApp.Project.Tracks[i].Events[0];
+				currentEvent = currentVegasApp.Project.Tracks[i].Events[0];
+			}
+			catch{
+				continue;
+			}
+			foreach (string j in arrTimeCodes){
+				string[] current_note = j.Split(',');//Parse "1,2" into {"1","2"}
+				var copiedEvent = currentEvent.Copy(currentVegasApp.Project.Tracks[i], Timecode.FromPositionString(current_note[0], RulerFormat.Seconds));
+				copiedEvent.AdjustStartLength(Timecode.FromPositionString(current_note[0], RulerFormat.Seconds), Timecode.FromPositionString(current_note[1], RulerFormat.Seconds), false);
+			}
+			currentVegasApp.Project.Tracks[i].Events.Remove(sourceEvent);
+		}		
+	}			
+}
