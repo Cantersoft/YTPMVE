57d610e9bf6299ce89a7ccb0f263aa4a45d289e8
Nath_SoM <49078943+NathSoM@users.noreply.github.com>
2021-08-08 11:11:38 +0200

    better exit function + error messages in cs script instead

--- YTPMVE.py
+++ YTPMVE.py
@@ -1,7 +1,14 @@
-import os
+import os, sys, subprocess
 from time import sleep as wait
 from os import path
-import sys
+
+try:
+    os.remove(path.expandvars('%USERPROFILE%\\AppData\\Local\\Temp\\YTPMVE\\errlog.txt')) # Deletes previous error log
+except FileNotFoundError:
+    pass
+
+isFrozen = None
+isPythonPresent = None
 
 if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'): # This code checks if the script is running in a bundled EXE or from the normal interpreter
     isFrozen = True
@@ -11,11 +18,31 @@ else:
     print('Running from script')
 
 try:
+    subprocess.run(["python", "--version"]) # This checks if python is present
+except:
+    isPythonPresent = False
+    print("Python is not present")
+else:
+    isPythonPresent = True
+    print("Python is present")
+
+try:
     os.makedirs((path.expandvars(r'%USERPROFILE%/AppData/Local/Temp/YTPMVE/')))
 except FileExistsError:
     pass
 
 YTPMVE_file= open(path.expandvars(r'%USERPROFILE%/AppData/Local/Temp/YTPMVE/timestamps.txt'), 'w')
+ErrorLogFile = open(path.expandvars(r'%USERPROFILE%\\AppData\\Local\\Temp\\YTPMVE\\errlog.txt'), 'w')
+
+def exitScript(errMessage, exitCode, retryWithPythonScript):
+    r = True
+    r = retryWithPythonScript
+    ErrorLogFile.write(errMessage + "\n")
+    ErrorLogFile.write(str(exitCode) + "\n")
+    ErrorLogFile.write(str(r))
+    ErrorLogFile.close()
+    YTPMVE_file.close()
+    os._exit(exitCode)
 
 import tkinter.filedialog as tf
 import tkinter
@@ -23,41 +50,25 @@ import tkinter
 try:
     import mido
 except ModuleNotFoundError as error:
-    if isFrozen: # Do not print the messages if running from bundle
-        print("Trying again with the Python script instead...")
-        wait(5)
-        YTPMVE_file.close()
-        os._exit(1)
-    else:
-        print("Error"+"\n")
-        print("ModuleNotFoundError"+"\n")
-        print(str(error)+". Module missing. Install mido with the command \"pip install mido\" in the command prompt and try again.")
-        YTPMVE_file.close()
-        input("Press Enter to continue...")
-        os._exit(1)
+        exitScript("Module \"mido\" not found, install it with the command \"pip install mido\".", 1, False)
+
 
 
 root = tkinter.Tk()
 root.withdraw()
 MIDI_filename = tf.askopenfilename(initialdir = "/",title= "Select MIDI file", filetypes =[('MIDI', '*.mid')])
-print("Opened", MIDI_filename)
 root.destroy()
 
-MIDI_file=mido.MidiFile(MIDI_filename)
+try:
+    MIDI_file=mido.MidiFile(MIDI_filename)
+except FileNotFoundError:
+    exitScript("No file selected!", 1, False)
+else:
+    print("Opened", MIDI_filename)
+
 
 if not len(MIDI_file.tracks)-1 == 1 : # reject files with more than one track or zero tracks
-    if isFrozen: # Do not print the messages if running from bundle
-        print("Trying again with the Python script instead...")
-        wait(5)
-        YTPMVE_file.close()
-        os._exit(1)
-    else:
-        print("Error"+"\n")
-        print("TrackError"+"\n")
-        print("An error occurred because there are too many tracks in the MIDI file you selected. Currently, this script only supports 1 track.")
-        YTPMVE_file.close()
-        input("Press Enter to continue...")
-        os._exit(1)
+        exitScript("Too many tracks in MIDI File!", 1, False)
 
     
 current_time=0
@@ -98,13 +109,6 @@ for msg in MIDI_file: # Change this so that we don't look at the if statement ex
                     print()
                     print("   Matching note found in note_starts at index position", list_match)
                     break
-               
-            
-            
-           
-            
-
-
 
         
 #print(str(MIDI_time)) #Print all the note starts and lengths, optionally.
@@ -119,4 +123,6 @@ for i, j in enumerate(note_starts):#i becomes a counter, and j becomes the corre
     YTPMVE_file.write(str(note_starts[i][0])+",")#Save first argument, note start time
     YTPMVE_file.write(str(note_durations[i])+"\n")#Save second argument, note duration
 
-YTPMVE_file.close()
+
+
+exitScript("none", 0, False)
