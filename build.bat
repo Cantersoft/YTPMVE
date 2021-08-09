df29046e13f729ab824d57db00a653f1baa21ecb
Nath_SoM <49078943+NathSoM@users.noreply.github.com>
2021-08-09 18:23:29 +0200

    remove build.bat

--- build.bat
+++
@@ -1,7 +0,0 @@
-set v=18
-
-pyinstaller --onefile .\YTPMVE.py
-move .\dist\YTPMVE.exe .
-xcopy /y *.cs "%ProgramFiles%\VEGAS\VEGAS Pro %v%.0\Script Menu\YTPMVE"
-xcopy /y YTPMVE.py "%ProgramFiles%\VEGAS\VEGAS Pro %v%.0\Script Menu\YTPMVE"
-xcopy /y YTPMVE.exe "%ProgramFiles%\VEGAS\VEGAS Pro %v%.0\Script Menu\YTPMVE"
