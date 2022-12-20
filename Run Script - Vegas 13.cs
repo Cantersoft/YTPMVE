18ee41dce6ea08c4f0b424c7cc5d3f45839dbe05
Cantersoft <46493366+Cantersoft@users.noreply.github.com>
2022-12-19 19:37:32 -0500

    Added Auto-flipping!
    Video auto flipping feature added.

--- Run Script - Vegas 13.cs
+++ Run Script - Vegas 13.cs
@@ -1,5 +1,5 @@
 //YTPMVE
-//20220924
+//20221219
 using System;
 using System.IO;
 using System.Collections.Generic;
@@ -88,6 +88,11 @@ public class EntryPoint{
 		bool timestampsContainsNulls = false;
 		bool tracksMissing = false;
 		bool foundFirstEvent = false;
+		
+		//Options
+		
+		bool flip_x = true;
+		bool flip_y = false;
 			
 		
 		//This region of the code is now much stricter about what data is allowed into the second array. You can put lots of random crap in timestamps.txt now and it will still mostly work.
@@ -141,13 +146,15 @@ public class EntryPoint{
 			MessageBox.Show("No events exist in the timeline.", "Error" , MessageBoxButtons.OK, MessageBoxIcon.Error);
 			return;
 		}
+		
+		//Duplicate the clip across the timeline according to the data in the timecodes array.
 		foreach (string j in arrTimeCodes){
 			string[] current_note = j.Split(',');//Parse "1,2" into {"1","2"}
 			
 			try{
 				currentEvent = currentVegasApp.Project.Tracks[int.Parse(current_note[0])].Events[0];
 				var copiedEvent = currentEvent.Copy(currentVegasApp.Project.Tracks[int.Parse(current_note[0])], Timecode.FromPositionString(current_note[1], RulerFormat.Seconds));
-				copiedEvent.AdjustStartLength(Timecode.FromPositionString(current_note[1], RulerFormat.Seconds), Timecode.FromPositionString(current_note[2], RulerFormat.Seconds), false);
+				copiedEvent.AdjustStartLength(Timecode.FromPositionString(current_note[1], RulerFormat.Seconds), Timecode.FromPositionString(current_note[2], RulerFormat.Seconds), false);					
 			}
 			catch{
 				//missingTrackIndices.Add(int.Parse(current_note[0]));//remove duplicates
@@ -160,6 +167,36 @@ public class EntryPoint{
 		for (int i = 0; i < sourceEvents.Count; i++){
 			currentVegasApp.Project.Tracks[sourceEvents[i].Track.Index].Events.Remove(sourceEvents[i]);
 		}
+		
+		
+		//SelectEveryOtherEvent.cs + sykhro auto flips || In this second loop, all other effects would be applied.	
+        foreach (Track track in currentVegasApp.Project.Tracks){
+            bool selectThisEvent = false;
+            foreach (VideoEvent currentVideoEvent in track.Events){
+                if (selectThisEvent){
+                    currentVideoEvent.Selected = true;
+					
+					// Assign video vertexes to keyframes
+					VideoMotionVertex tl = currentVideoEvent.VideoMotion.Keyframes[0].TopLeft;
+					VideoMotionVertex tr = currentVideoEvent.VideoMotion.Keyframes[0].TopRight;
+					VideoMotionVertex bl = currentVideoEvent.VideoMotion.Keyframes[0].BottomLeft;
+					VideoMotionVertex br = currentVideoEvent.VideoMotion.Keyframes[0].BottomRight;
+					
+					if(flip_x){					
+						// Re-bound them, in order to produce a horizontal flip.
+						currentVideoEvent.VideoMotion.Keyframes[0].Bounds = new VideoMotionBounds(tr, tl, bl, br);
+					}
+					
+					if(flip_y){
+						// Re-bound them, in order to produce a vertical flip.
+						currentVideoEvent.VideoMotion.Keyframes[0].Bounds = new VideoMotionBounds(bl, br, tr, tl);
+					}					
+                }
+                selectThisEvent = !selectThisEvent;
+            }
+        }
+
+		
 		if(tracksMissing){
 			//MessageBox.Show("Note data from tracks " + string.Join( ", ", missingTrackIndices.ToArray) + " was left out because there are not enough tracks.", "Warning" , MessageBoxButtons.OK, MessageBoxIcon.Warning); //Doesn't work because HashSet is broken
 			MessageBox.Show("Note data from some tracks was left out because there are not enough tracks.", "Warning" , MessageBoxButtons.OK, MessageBoxIcon.Warning);
